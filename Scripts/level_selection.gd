7e5da3ede599a4a114dad5ea2fb856556b41c2aa
Axel Goris <axelgoris99@gmail.com>
2024-01-13 23:40:26 +1030

    feat: Menu UI
    Added a level selection
    Added scene transition
    Added options menu

---
+++ Scripts/level_selection.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _on_lvl_1_pressed():
+	SceneTransition.change_scene_to_file("res://Scenes/lvls/lvl1.tscn")
+
+
+func _on_lvl_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_lvl_3_pressed():
+	pass # Replace with function body.
+
+
+func _on_lvl_4_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_pressed():
+	SceneTransition.change_scene_to_file("res://Scenes/UI/menu.tscn")
---
+++ Scripts/menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_start_button_pressed():
+	SceneTransition.change_scene_to_file("res://Scenes/UI/level_selection.tscn")
+
+func _on_options_button_pressed():
+	SceneTransition.change_scene_to_file("res://Scenes/UI/options.tscn")
+	
+func _on_quit_button_pressed():
+	get_tree().quit()
+
