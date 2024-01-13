71cb56e651e4d5bd50464ef5fdd2194038e2646e
Axel Goris <axelgoris99@gmail.com>
2024-01-13 17:44:51 +1030

    fix: character changes

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,3 +1,4 @@
+class_name Character
 extends Node
 
 var current_chair
@@ -13,3 +14,26 @@ func remove_chair():
 		return
 	current_chair.set_character(null)
 	current_chair = null
+
+# Path to the character's profile, .dch file
+var profile_path : String = ""
+# Store the intro texts
+var intro_dialog_events : Array = []
+# Store the liked objects and assorted texts
+var likes_dialog_events : Dictionary = {}
+# Store the disliked objects and assorted texts
+var dislikes_dialog_events : Dictionary = {}
+# Records which likes/dislikes the player has become aware of
+var known_objects : Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# Add objects to the list of objects the player is aware of 
+func add_known_objects(objects : Array):
+	known_objects.append_array(objects)
