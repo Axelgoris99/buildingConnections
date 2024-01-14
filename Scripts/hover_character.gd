ba5cb8ba7185771e2314ab5cdfc40a98f3cd3784
Axel Goris <axelgoris99@gmail.com>
2024-01-14 15:48:01 +1030

    feat: score + pause_menu
    Also inverted the score calculation to be more intuitive.

---
+++ Scripts/hover_character.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var current_character: Character
+
+@export var name_label: RichTextLabel
+@export var dislikes: VBoxContainer
+@export var likes: VBoxContainer
+
+func set_character(character: Character):
+	current_character = character
+	name_label.text = character.name.to_upper()
+	# Not efficient but meh, no time
+	for like in character.likes:
+		if(like in character.known_objects):
+			add_node(likes, like)
+		else:
+			add_unknown_node(likes)
+	for dislike in character.dislikes:
+		if(dislike in character.known_objects):
+			add_node(dislikes, dislike)
+		else:
+			add_unknown_node(dislikes)
+
+func add_unknown_node(parent_node: Node):
+	var text_label = RichTextLabel.new()
+	text_label.text = "?"
+	text_label.fit_content = true
+	parent_node.add_child(text_label)
+	
+func add_node(parent_node: Node, text: String):
+	var text_label = RichTextLabel.new()
+	text_label.text = text.to_upper()
+	text_label.fit_content = true
+	parent_node.add_child(text_label)
---
+++ Scripts/score_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@export var score_node: RichTextLabel
+
+func _on_retry_pressed():
+	get_tree().paused = false
+	SceneTransition.reload_scene()
+
+func _on_main_menu_pressed():
+	get_tree().paused = false
+	SceneTransition.change_scene_to_file("res://Scenes/UI/menu.tscn")
