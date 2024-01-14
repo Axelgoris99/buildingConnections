37f73f06b48cc0ae6299150caaf3a1dfdd1f685b
Axel Goris <axelgoris99@gmail.com>
2024-01-14 23:17:52 +1030

    feat: added a tip for lvl1
    fixed the last bugs

--- Scripts/DialogManager.gd
+++ Scripts/DialogManager.gd
@@ -6,21 +6,20 @@ extends Node
 func run_dialog(character : Character, likes : Array, dislikes : Array):
 	var timeline : DialogicTimeline = DialogicTimeline.new()
 	var events : Array = []
-	events.append('[style name="SpeakerTextBox"]')
 	var text_event = DialogicTextEvent.new()
-	text_event.text = '"' + character.name +': '+ character.intro_dialog_events[randi() % character.intro_dialog_events.size()] + '"'
+	text_event.text = character.intro_dialog_events[randi() % character.intro_dialog_events.size()]
 	var charprofile : DialogicCharacter = load("res://dialogs/" + character.name + ".dch")
 	charprofile.display_name = character.display_name
 	text_event.character = charprofile#load("res://dialogs/" + character.name + ".dch")
 	events.append(text_event)
 	for like in likes:
 		var like_event = DialogicTextEvent.new()
-		like_event.text = '"' + character.likes_dialog_events[like] + '"'
+		like_event.text = character.likes_dialog_events[like] 
 		text_event.character = charprofile
 		events.append(like_event)
 	for dislike in dislikes:
 		var dislike_event = DialogicTextEvent.new()
-		dislike_event.text = '"' + character.dislikes_dialog_events[dislike] + '"'
+		dislike_event.text =  character.dislikes_dialog_events[dislike]
 		text_event.character = charprofile
 		events.append(dislike_event)
 	timeline.events = events
